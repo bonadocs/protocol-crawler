11f22ea583c5d0698e5b3fac600ef32417afbc2e
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2024-01-11 20:54:29 +0100

    Implement stub for protocol crawler
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+﻿import fs from 'fs'
+import path from 'path'
+
+import puppeteer, { Browser } from 'puppeteer'
+
+import { crawl } from './crawler'
+import type { CrawlResult, ProcessContext } from './crawler/types'
+import protocolUrls from './protocol-urls.json'
+
+function writeResults(results: CrawlResult[]) {
+  const resultPath = path.resolve('./crawled-protocols.json')
+  results = JSON.parse(JSON.stringify(results)).filter(Boolean)
+  const data = JSON.stringify(results, null, 2)
+  fs.writeFileSync(resultPath, data)
+}
+
+async function crawlProtocols(
+  browser: Browser,
+  urls: string[],
+  maxSimultaneousCrawls = 30,
+): Promise<CrawlResult[]> {
+  const queue = [...urls]
+  const inProgress = new Set<string>()
+  const completed = new Set<string>()
+
+  const existingPages = await browser.pages()
+  for (const page of existingPages.slice(1)) {
+    await page.close()
+  }
+
+  const results: CrawlResult[] = []
+  const context: ProcessContext = {
+    visited: new Set(),
+  }
+  while (queue.length > 0 || completed.size < urls.length) {
+    while (inProgress.size < maxSimultaneousCrawls && queue.length > 0) {
+      const url = queue.shift()!
+      inProgress.add(url)
+      const page = await browser.newPage()
+      console.time(`Crawled ${url}`)
+      crawl(page, url, context).then((result) => {
+        results.push(result)
+        console.timeEnd(`Crawled ${url}`)
+        console.log('Total count:', results.length)
+        writeResults(results)
+        completed.add(url)
+        inProgress.delete(url)
+      })
+    }
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+  }
+  return results
+}
+
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    defaultViewport: null,
+  })
+  try {
+    const results = await crawlProtocols(
+      browser,
+      protocolUrls.filter((u) => samples.some((s) => u.includes(s))),
+    )
+    console.log('results:', JSON.stringify(results, null, 2))
+  } finally {
+    await browser.close()
+  }
+}
+
+main().catch((err) => {
+  console.error(err)
+  throw err
+})
+
+const samples = [
+  'uniswap',
+  'gho',
+  'aave',
+  'compound',
+  'convex',
+  'curve',
+  'balancer',
+  'lido',
+]
