9dda9b28a4dd586dfa74be7366dffbd148ec35db
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2024-01-13 17:54:31 +0100

    Implement deployment extraction from table data
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/crawler/contracts/table/strategies/index.ts
@@ -0,0 +1,36 @@
+﻿import type { ContractDeployment } from '../../../types'
+import type { Table } from '../types'
+
+import {
+  contractAddressStrategy,
+  isContractAddressStrategy,
+} from './contractAddressTableStrategy'
+import {
+  contractNetworkStrategy,
+  isContractNetworkStrategy,
+} from './contractNetworkTableStrategy'
+import { determineTableStrategy } from './util'
+
+export async function extractContractDeploymentsFromTable(
+  table: Table,
+): Promise<ContractDeployment[] | undefined> {
+  const strategy = determineTableStrategy(table)
+  if (!strategy) {
+    return undefined
+  }
+
+  switch (strategy) {
+    case 'contractAddress':
+      return contractAddressStrategy(table)
+    case 'contractNetwork':
+      return contractNetworkStrategy(table)
+  }
+}
+
+export {
+  determineTableStrategy,
+  isContractAddressStrategy,
+  contractAddressStrategy,
+  isContractNetworkStrategy,
+  contractNetworkStrategy,
+}
---
+++ src/crawler/contracts/table/strategies/util.ts
@@ -0,0 +1,31 @@
+﻿import type { Table } from '../types'
+
+import { isContractAddressStrategy } from './contractAddressTableStrategy'
+import { isContractNetworkStrategy } from './contractNetworkTableStrategy'
+
+/**
+ * For each strategy, we only need to support the row case.
+ * We flip the table to get the column case.
+ */
+export type TableStrategy = 'contractAddress' | 'contractNetwork'
+
+/**
+ * Determines the strategy for extracting contract deployments
+ * from a deployments table. The input is guaranteed to be a
+ * deployments table.
+ *
+ * @param table
+ */
+export function determineTableStrategy(
+  table: Table,
+): TableStrategy | undefined {
+  if (isContractNetworkStrategy(table)) {
+    return 'contractNetwork'
+  }
+
+  if (isContractAddressStrategy(table)) {
+    return 'contractAddress'
+  }
+
+  return undefined
+}
