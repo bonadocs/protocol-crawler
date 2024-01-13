b146c5f2dad3e2e0a91d6a2eb156fe01c824dbbd
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2024-01-13 17:59:16 +0100

    Implement contract deployment extraction with tables and lists
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/crawler/contracts/index.ts
@@ -0,0 +1,19 @@
+﻿import type { Page } from 'puppeteer'
+
+import type { ContractDeployment } from '../types'
+
+import { extractContractDeploymentsFromPageLists } from './list'
+import { extractDeploymentsFromPageTables } from './table'
+
+export { uniqueContracts } from './util'
+export async function extractContracts(
+  page: Page,
+): Promise<ContractDeployment[]> {
+  if (!page || page.isClosed()) {
+    return []
+  }
+
+  const tableDeployments = await extractDeploymentsFromPageTables(page)
+  const listDeployments = await extractContractDeploymentsFromPageLists(page)
+  return [...tableDeployments.flat(), ...listDeployments]
+}
